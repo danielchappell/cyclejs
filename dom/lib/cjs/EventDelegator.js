a9571e4805fdfaf8d55ac1e1e68ebffc82cf0063
Daniel Chappell <daniel.chappell@q2ebanking.com>
2018-04-17 13:00:18 -0500

    commit compiled lib for cycle/dom

---
+++ dom/lib/cjs/EventDelegator.js
@@ -0,0 +1,200 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var xstream_1 = require("xstream");
+var ScopeChecker_1 = require("./ScopeChecker");
+var utils_1 = require("./utils");
+var matchesSelector_1 = require("./matchesSelector");
+var fromEvent_1 = require("./fromEvent");
+/**
+ * Finds (with binary search) index of the destination that id equal to searchId
+ * among the destinations in the given array.
+ */
+function indexOf(arr, searchId) {
+    var minIndex = 0;
+    var maxIndex = arr.length - 1;
+    var currentIndex;
+    var current;
+    while (minIndex <= maxIndex) {
+        currentIndex = ((minIndex + maxIndex) / 2) | 0; // tslint:disable-line:no-bitwise
+        current = arr[currentIndex];
+        var currentId = current.id;
+        if (currentId < searchId) {
+            minIndex = currentIndex + 1;
+        }
+        else if (currentId > searchId) {
+            maxIndex = currentIndex - 1;
+        }
+        else {
+            return currentIndex;
+        }
+    }
+    return -1;
+}
+/**
+ * HTMLSlotElement aware contains:
+ *  We (cycle/dom) don't want stop propagation on events belonging to a different (light) DOM just passing through.
+ *  The implementer however might want to stop propagation depending on application logic.
+ */
+function originatesInSlots(origin, target) {
+    var slots = origin.querySelectorAll('slot');
+    var slotIndex = 0;
+    for (; slotIndex < slots.length; slotIndex++) {
+        var currentSlot = slots[slotIndex];
+        var lightNodes = currentSlot.assignedNodes && currentSlot.assignedNodes();
+        var nodeIndex = 0;
+        for (; nodeIndex < lightNodes.length; nodeIndex++) {
+            if (lightNodes[nodeIndex].contains(target)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+/**
+ * Manages "Event delegation", by connecting an origin with multiple
+ * destinations.
+ *
+ * Attaches a DOM event listener to the DOM element called the "origin",
+ * and delegates events to "destinations", which are subjects as outputs
+ * for the DOMSource. Simulates bubbling or capturing, with regards to
+ * isolation boundaries too.
+ */
+var EventDelegator = /** @class */ (function () {
+    function EventDelegator(origin, eventType, useCapture, isolateModule, preventDefault) {
+        if (preventDefault === void 0) { preventDefault = false; }
+        var _this = this;
+        this.origin = origin;
+        this.eventType = eventType;
+        this.useCapture = useCapture;
+        this.isolateModule = isolateModule;
+        this.preventDefault = preventDefault;
+        this.destinations = [];
+        this._lastId = 0;
+        if (preventDefault) {
+            if (useCapture) {
+                this.listener = function (ev) {
+                    fromEvent_1.preventDefaultConditional(ev, preventDefault);
+                    _this.capture(ev);
+                };
+            }
+            else {
+                this.listener = function (ev) {
+                    fromEvent_1.preventDefaultConditional(ev, preventDefault);
+                    _this.bubble(ev);
+                };
+            }
+        }
+        else {
+            if (useCapture) {
+                this.listener = function (ev) { return _this.capture(ev); };
+            }
+            else {
+                this.listener = function (ev) { return _this.bubble(ev); };
+            }
+        }
+        origin.addEventListener(eventType, this.listener, useCapture);
+    }
+    EventDelegator.prototype.updateOrigin = function (newOrigin) {
+        this.origin.removeEventListener(this.eventType, this.listener, this.useCapture);
+        newOrigin.addEventListener(this.eventType, this.listener, this.useCapture);
+        this.origin = newOrigin;
+    };
+    /**
+     * Creates a *new* destination given the namespace and returns the subject
+     * representing the destination of events. Is not referentially transparent,
+     * will always return a different output for the same input.
+     */
+    EventDelegator.prototype.createDestination = function (namespace) {
+        var _this = this;
+        var id = this._lastId++;
+        var selector = utils_1.getSelectors(namespace);
+        var scopeChecker = new ScopeChecker_1.ScopeChecker(utils_1.getFullScope(namespace), this.isolateModule);
+        var subject = xstream_1.default.create({
+            start: function () { },
+            stop: function () {
+                if ('requestIdleCallback' in window) {
+                    requestIdleCallback(function () {
+                        _this.removeDestination(id);
+                    });
+                }
+                else {
+                    _this.removeDestination(id);
+                }
+            },
+        });
+        var destination = { id: id, selector: selector, scopeChecker: scopeChecker, subject: subject };
+        this.destinations.push(destination);
+        return subject;
+    };
+    /**
+     * Removes the destination that has the given id.
+     */
+    EventDelegator.prototype.removeDestination = function (id) {
+        var i = indexOf(this.destinations, id);
+        i >= 0 && this.destinations.splice(i, 1); // tslint:disable-line:no-unused-expression
+    };
+    EventDelegator.prototype.capture = function (ev) {
+        var n = this.destinations.length;
+        for (var i = 0; i < n; i++) {
+            var dest = this.destinations[i];
+            if (matchesSelector_1.matchesSelector(ev.target, dest.selector)) {
+                dest.subject._n(ev);
+            }
+        }
+    };
+    EventDelegator.prototype.bubble = function (rawEvent) {
+        var origin = this.origin;
+        if (!origin.contains(rawEvent.currentTarget)) {
+            return;
+        }
+        var roof = origin.parentElement;
+        var ev = this.patchEvent(rawEvent);
+        for (var el = ev.target; el && el !== roof; el = el.parentElement) {
+            if (!(origin.contains(el) || originatesInSlots(origin, ev.target))) {
+                ev.stopPropagation();
+            }
+            if (ev.propagationHasBeenStopped) {
+                return;
+            }
+            this.matchEventAgainstDestinations(el, ev);
+        }
+    };
+    EventDelegator.prototype.patchEvent = function (event) {
+        var pEvent = event;
+        pEvent.propagationHasBeenStopped = false;
+        var oldStopPropagation = pEvent.stopPropagation;
+        pEvent.stopPropagation = function stopPropagation() {
+            oldStopPropagation.call(this);
+            this.propagationHasBeenStopped = true;
+        };
+        return pEvent;
+    };
+    EventDelegator.prototype.matchEventAgainstDestinations = function (el, ev) {
+        var n = this.destinations.length;
+        for (var i = 0; i < n; i++) {
+            var dest = this.destinations[i];
+            if (!dest.scopeChecker.isDirectlyInScope(el)) {
+                continue;
+            }
+            if (matchesSelector_1.matchesSelector(el, dest.selector)) {
+                this.mutateEventCurrentTarget(ev, el);
+                dest.subject._n(ev);
+            }
+        }
+    };
+    EventDelegator.prototype.mutateEventCurrentTarget = function (event, currentTargetElement) {
+        try {
+            Object.defineProperty(event, "currentTarget", {
+                value: currentTargetElement,
+                configurable: true,
+            });
+        }
+        catch (err) {
+            console.log("please use event.ownerTarget");
+        }
+        event.ownerTarget = currentTargetElement;
+    };
+    return EventDelegator;
+}());
+exports.EventDelegator = EventDelegator;
+//# sourceMappingURL=EventDelegator.js.map
